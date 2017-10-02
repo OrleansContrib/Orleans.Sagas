c205f3021d6241f9608c7db3cca4cf480d8f3d03
Roger Creyke <creyke@gmail.com>
2017-10-02 13:05:01 +0100

    Changed BUILD to APPVEYOR_BUILD_NUMBER.

--- Build.ps1
+++ Build.ps1
@@ -1,3 +1,3 @@
 dotnet restore ./Orleans.Sagas/Orleans.Sagas.csproj --source https://dotnet.myget.org/F/orleans-prerelease/api/v3/index.json --source https://api.nuget.org/v3/index.json
 dotnet publish ./Orleans.Sagas/Orleans.Sagas.csproj
-dotnet pack ./Orleans.Sagas/Orleans.Sagas.csproj /p:PackageVersion=0.0.'$env:BUILD'-pre
+dotnet pack ./Orleans.Sagas/Orleans.Sagas.csproj /p:PackageVersion=0.0.'$env:APPVEYOR_BUILD_NUMBER'-pre
